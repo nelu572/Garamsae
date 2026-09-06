6a98673966b39624914355746b0ae077ac45b421
nelu572 <s25031@gsm.hs.kr>
2026-09-06 22:39:15 +0900

    feat: 상수 스크립트, 폴더 추가

---
+++ Assets/Scripts/Values/Constants.cs
@@ -0,0 +1,8 @@
+public static class Constants
+{
+    public static class Player
+    {
+        public const float MoveSpeed = 8.0f;
+        public const float JumpForce = 8.0f;
+    }
+}
---
+++ Assets/Scripts/Values/Scenes.cs
@@ -0,0 +1,6 @@
+public static class SceneNames
+{
+    public const string PrologueIntro = "Prologue_01_Intro";
+    public const string PrologueHome = "Prologue_02_Home";
+    public const string PrologueEscape = "Prologue_03_Escape";
+}
